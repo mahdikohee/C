bcb8916b41957b939a4d41a170b096e3c5f403f6
mahdikohee <mahdikohee@gmail.com>
2025-06-15 14:12:38 +0600

    Create basic_skills.c

---
+++ basic_skills.c
@@ -0,0 +1,10 @@
+//A simple example to show time 
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t now;
+    time(&now);
+    printf("Current time: %s", ctime(&now));
+    return 0;
+}
